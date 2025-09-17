3916122809fdea5d195cd16b77aaeffe676d14b6
jonathan <johh.almeida@gmail.com>
2025-09-17 14:45:12 -0300

    week2_code_fixed

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -12,7 +12,13 @@ public class PriorityQueueTests
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("low_priority", 1);
+        priorityQueue.Enqueue("medium_priority", 2);
+        priorityQueue.Enqueue("high_priority", 3);
+
+        Assert.AreEqual("high_priority", priorityQueue.Dequeue());
+        Assert.AreEqual("medium_priority", priorityQueue.Dequeue());
+        Assert.AreEqual("low_priority", priorityQueue.Dequeue());;
     }
 
     [TestMethod]
@@ -22,7 +28,13 @@ public class PriorityQueueTests
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("first", 5);
+        priorityQueue.Enqueue("second", 5);
+        priorityQueue.Enqueue("third", 5);
+
+        Assert.AreEqual("first", priorityQueue.Dequeue());
+        Assert.AreEqual("second", priorityQueue.Dequeue());
+        Assert.AreEqual("third", priorityQueue.Dequeue());
     }
 
     // Add more test cases as needed below.
